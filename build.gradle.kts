003744454cba367677d7068e060ce2a39dad3b3b
Bartosz Kanadys <kanadysbartosz@gmail.com>
2025-03-26 14:12:28 +0100

    Add .env file feature

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.github.cdimascio:dotenv-java:3.0.0")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
