4b1d700d202eca385d138e7dff64d3aabf033598
Bartosz Kanadys <kanadysbartosz@gmail.com>
2025-03-26 15:28:17 +0100

    Update movie repository
    add pageable search

--- src/main/kotlin/com/movie/restApi/repository/MovieRepository.kt
+++ src/main/kotlin/com/movie/restApi/repository/MovieRepository.kt
@@ -1,10 +1,12 @@
 package com.movie.restApi.repository
 
 import com.movie.restApi.model.Movie
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.Optional
 
 interface MovieRepository: MongoRepository<Movie, String> {
     fun findByTitleContainingIgnoreCase(title: String): List<Movie>
     override fun findById(id: String): Optional<Movie>
+    fun findByTitleRegex(title: String, pageable: Pageable)
 }
