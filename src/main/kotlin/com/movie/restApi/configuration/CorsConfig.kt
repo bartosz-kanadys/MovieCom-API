cd42eeedba6207b149ab2e75ecf532099eebd658
Bartosz Kanadys <kanadysbartosz@gmail.com>
2025-04-06 21:30:48 +0200

    cors file convert from java to kotlin

--- src/main/kotlin/com/movie/restApi/configuration/CorsConfig.kt
+++ src/main/kotlin/com/movie/restApi/configuration/CorsConfig.kt
@@ -1,24 +1,22 @@
-package com.movie.restApi.configuration;
+package com.movie.restApi.configuration
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-public class CorsConfig {
-
+class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+    fun corsConfigure(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**") // Dopuszcza wszystkie endpointy
-                        .allowedOrigins("http://localhost:5173") // Zezwala na dostęp z tej domeny
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Dopuszcza określone metody HTTP
-                        .allowedHeaders("*") // Dopuszcza wszystkie nagłówki
-                        .allowCredentials(true); // Zezwala na uwierzytelnienie (np. ciasteczka)
+                    .allowedOrigins("http://localhost:5173") // Zezwala na dostęp z tej domeny
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Dopuszcza określone metody HTTP
+                    .allowedHeaders("*") // Dopuszcza wszystkie nagłówki
+                    .allowCredentials(true) // Zezwala na uwierzytelnienie (np. ciasteczka)
             }
-        };
+        }
     }
 }
